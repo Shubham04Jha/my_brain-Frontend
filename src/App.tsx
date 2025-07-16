860bdd0cad1949f351a4e3332a84fd2d5de8fa8d
Shubham04Jha <shubhamjha70491@gmail.com>
2025-07-16 02:43:05 +0000

    added Responsive shape shifting button for different screen sizes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import './App.css'
 import { Button } from './components/ui/Button'
 import { ProfileCard } from './components/ui/ProfileCard'
+import { ResponsiveButton } from './components/ui/ResponsiveButton'
+import { shareSvg } from "./assets/icons/share";
 
 function App() {
 
   return (
     <div className='dark
-        
+        flex items-center justify-center 
         dark:text-text-white text-text-black 
         dark:bg-background-black bg-background-white
-        my-auto h-screen
+        h-screen
         px-10 py-2'>
-      <ProfileCard variant='extended' username={'shubham@123'}/>
+      {/* <ProfileCard variant='extended' username={'shubham@123'}/> */}
+      <ResponsiveButton text="Share Brain" size='md' padding='px-4 py-2' startIcon={shareSvg}/>
     </div>
   )
 }
---
+++ src/components/ui/ResponsiveButton.tsx
@@ -0,0 +1,50 @@
+import type { ReactNode } from "react";
+import useResponsive from "../../hooks/useResponsive";
+
+
+interface ButtonProps{
+    size: 'sm'|'md'|'lg'
+    startIcon? : ReactNode;
+    text: string;
+    endIcon? : ReactNode;
+    width?: string;
+    padding?: string;
+}
+
+const IconSize = {
+    sm:'w-4',
+    md:'w-8',
+    lg:'w-16'
+}
+
+const TextSize = {
+    sm:'text-xl',
+    md:'text-2xl',
+    lg:'text-4xl'
+}
+
+const defaultStyle = `dark:bg-secondary-black bg-secondary-white 
+                        rounded-md inline-flex`
+
+export const ResponsiveButton = ({
+    size='md',
+    startIcon,
+    text,
+    endIcon,
+    width,
+    padding,
+}: ButtonProps)=>{
+    const {isMobile} = useResponsive();
+    return(
+        <div className={`${defaultStyle} ${width} ${padding} `}>
+            <div className="justify-center flex gap-4 items-center">
+                {startIcon&&<div className={`${IconSize[size]}`}>{startIcon}</div>}
+                {!isMobile&&
+                <div className={` flex items-center -mt-1 ${TextSize[size]}`}>
+                    {text}
+                </div>}
+                {endIcon&&<div className={`${IconSize[size]}`}>{endIcon}</div>}
+            </div>
+        </div>
+    )
+}
