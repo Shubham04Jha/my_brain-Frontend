afe04b753c80cab86782efe38ed2c0e7b62e0990
Shubham04Jha <shubhamjha70491@gmail.com>
2025-08-07 06:52:08 +0000

    added brainlogo in pageNotFound

--- src/components/FallBack.tsx
+++ src/components/FallBack.tsx
@@ -1,9 +1,16 @@
+import { OpenBrainLogo } from "./ui/brainlogo"
 
 
 export const FallBack = ()=>{
     return(
+        <>
+        <div className="absolute left-4 top-4">
+            <OpenBrainLogo /> 
+        </div>
         <div className="w-full h-full">
             <h1>Something went wrong...</h1>
         </div>
+        </>
+        
     )
 }
--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,12 +1,20 @@
+import { OpenBrainLogo } from "../components/ui/brainlogo"
 
 
 export const PageNotFound = ()=>{
     return(
+        <>
+        <div className="absolute left-4 top-4">
+            <OpenBrainLogo /> 
+        </div>
         <div className="dark:bg-background-black bg-background-white w-full h-screen flex justify-center items-center">
+            
             <div>
                 <h1>404 Page not found...</h1>
-                <h3>Try checking the routes...</h3>
+                <h3>Try checking the url...</h3>
             </div>
         </div>
+        </>
+        
     )
 }
